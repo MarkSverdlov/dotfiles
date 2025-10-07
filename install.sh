c6fc1636e1e00e513940b7e776ccec2a9de2aa87
MarkSverdlov <marksve039@gmail.com>
2025-10-08 02:56:26 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+ln -s ~/dotfiles/vimrc ~/.vimrc 2> /dev/null
+ln -s ~/dotfiles/bashrc ~/.bashrc 2> /dev/null
+ln -s ~/dotfiles/chktexrc ~/.chktexrc 2> /dev/null
+ln -s ~/dotfiles/emacs ~/.emacs 2> /dev/null
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf 2> /dev/null
+ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua 2> /dev/null
+ln -s ~/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc 2> /dev/null
