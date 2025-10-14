7bb9b90fbeb9c1c3d5dae9746d194181e26e39ef
MarkSverdlov <marksve039@gmail.com>
2025-10-14 12:48:41 +0300

    Add kitty configuration

--- install.sh
+++ install.sh
@@ -8,3 +8,4 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf 2> /dev/null
 ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua 2> /dev/null
 ln -s ~/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc 2> /dev/null
 ln -s ~/dotfiles/profile ~/.profile 2> /dev/null
+ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf 2> /dev/null
