18775cd73ddf69d01d8d2ad83234f97c351c8626
MarkSverdlov <marksve039@gmail.com>
2025-10-14 13:01:01 +0300

    Add error messages in cases installation fails

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-ln -s ~/dotfiles/vimrc ~/.vimrc 2> /dev/null
-ln -s ~/dotfiles/bashrc ~/.bashrc 2> /dev/null
-ln -s ~/dotfiles/chktexrc ~/.chktexrc 2> /dev/null
-ln -s ~/dotfiles/emacs ~/.emacs 2> /dev/null
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf 2> /dev/null
-ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua 2> /dev/null
-ln -s ~/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc 2> /dev/null
-ln -s ~/dotfiles/profile ~/.profile 2> /dev/null
-ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf 2> /dev/null
+ln -s ~/dotfiles/vimrc ~/.vimrc 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install vimrc"
+ln -s ~/dotfiles/bashrc ~/.bashrc 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install bashrc"
+ln -s ~/dotfiles/chktexrc ~/.chktexrc 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install chktexrc"
+ln -s ~/dotfiles/emacs ~/.emacs 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install emacs"
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install tmux.conf"
+ln -s ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install nvim/init.lua"
+ln -s ~/dotfiles/zathura/zathurarc ~/.config/zathura/zathurarc 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install zathura/zathurarc"
+ln -s ~/dotfiles/profile ~/.profile 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install profile"
+ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf 2> /dev/null || echo -e "\e[31mError\e[0m: Failed to install kitty.conf"
