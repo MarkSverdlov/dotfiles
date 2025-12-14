0ea271758fd03ff1e88e45eab37cbd1868c67323
Mark Sverdlov <marksve039@gmail.com>
2025-12-14 11:37:03 +0200

    Change sh REPL to bash instead of zsh
    Usually, I expect to script bash, rather then zsh, so this is the
    more useful REPL.

--- init.lua
+++ init.lua
@@ -177,7 +177,7 @@ iron.setup {
       sh = {
         -- Can be a table or a function that
         -- returns a table (see below)
-        command = {"zsh"}
+        command = {"bash"}
       },
       python = {
         command = { "ipython", "--no-autoindent"},
